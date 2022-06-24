6e0ff3642278d3fb157042cc969e02263f8ba11f
Erwan Chesneau <erwanchesneau@gmail.com>
2022-06-24 18:22:21 +0200

    Code nettoyé à en respectant le style PEP8

--- main.py
+++ main.py
@@ -1,65 +1,85 @@
 #!/bin/python3.6
 
+"""
+Module to create sklearn pipeline to predict tags of Stackoverflow question
+"""
 #=========================
-# Python's Module Loading 
+# Python's Module Loading
 #=========================
 import warnings
-warnings.filterwarnings("ignore")
 import logging
-logging.disable(logging.WARNING)
-
 import argparse
 import os
-import pickle
 import re
+import sys
+import pickle
+import joblib
 import nltk
 import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
-from sklearn.pipeline import Pipeline 
-from sklearn.svm import SVC
+import sklearn.pipeline
+import sklearn.svm
 from sklearn.metrics import accuracy_score, precision_score
+from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
-tf.get_logger().setLevel('INFO')
 import tensorflow_hub as hub
 
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+warnings.filterwarnings("ignore")
+logging.disable(logging.WARNING)
+tf.get_logger().setLevel('INFO')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 #=========================
 # functions
 #=========================
-def clean_tag_rm(l, rm_list=[]) :
+def clean_tag_rm(ltag, rm_list=None) :
     """
     function to remove tag in the tag list
     parameters : l list of tags
                  rm_list : list of tags to be removed
     returns : list : new list of tags
     """
-    return list(set(l)-set(rm_list))
+    if rm_list is None :
+        rm_list = []
+    return list(set(ltag)-set(rm_list))
 
-def clean_tag_keep(l, tags=[]) :
+def clean_tag_keep(ltag, keep_tags=None) :
     """
     function to keep only tag in the tag list
     parameters : l list of tags
                  list : list of tags to keep
     returns : list : new list of tags
     """
-    return [t for t in l if t in tags]
+    if keep_tags is None :
+        keep_tags = []
+    return [t for t in ltag if t in keep_tags]
 
 def tokenizer(txt) :
+    """
+    function to tokenize question of stackoverflow without lemmatation
+    Parameters : txt : str
+    returns : str
+    """
     tag_tokenizer = nltk.RegexpTokenizer(r'</?(?:b|p)>', gaps=True)
     txt_tokenizer = nltk.RegexpTokenizer(r'\w+')
 
     txt = ''.join([i for i in txt if not i.isdigit()])
     txt = re.sub(r'_+', ' ', txt)
-    
+
     tokens = txt_tokenizer.tokenize(' '.join(tag_tokenizer.tokenize(txt.lower())))
     return ' '.join(tokens)
 
 def use_features(sentences, embedding, batch_size=16):
+    """
+    function to create USE features 
+    Parameters : sentences : str
+                 embedding : tensorflow model
+                 batch_size : int
+    Returns : features : np.array
+    """
     for step in range(len(sentences)//batch_size) :
         idx = step*batch_size
         feat = embedding(sentences[idx:idx+batch_size])
@@ -68,24 +88,32 @@ def use_features(sentences, embedding, batch_size=16):
         else :
             features = np.concatenate((features,feat))
     return features
-    
+
 class UseTransformer() :
+    """
+    Class to transform questions of stackoverflow to USE features
+    """
     def __init__(self, tokenizer=None, embedding=None, batch_size=1) :
         self.batch_size = batch_size
-        self.X_tokenized = None
+        self.x_tokenized = None
         if tokenizer is None :
             self.tokenizer = self.dummy
         else :
             self.tokenizer = tokenizer
         if embedding is not None :
             self.embedding = embedding
-        else : 
+        else :
             print("Error : Please give me embedding")
-            exit()
+            sys.exit()
 
     def transform(self, X, y=None) :
-        self.X_tokenized = X.apply(self.tokenizer)
-        sentences = self.X_tokenized.to_list()
+        """
+        Method to transform str
+        Parameters : X : pd.Series
+        returns : np.array
+        """
+        self.x_tokenized = X.apply(self.tokenizer)
+        sentences = self.x_tokenized.to_list()
         for step in range(len(sentences)//self.batch_size) :
             idx = step*self.batch_size
             feat = self.embedding(sentences[idx:idx+self.batch_size])
@@ -94,8 +122,13 @@ class UseTransformer() :
             else :
                 features = np.concatenate((features,feat))
         return features
-    
+
     def fit(self, X, y=None) :
+        """
+        Method needed in sklearn pipeline 
+        Do nothing
+        return self
+        """
         return self
 
     def dummy(self, txt):
@@ -106,7 +139,7 @@ class UseTransformer() :
 
 if __name__ == "__main__" :
     #=========================
-    # Args reading 
+    # Args reading
     #=========================
     parser = argparse.ArgumentParser(description='Create clustering model '+\
                                      'for questions of StackOverFlow '+\
@@ -118,7 +151,7 @@ if __name__ == "__main__" :
                         action="store_true")
     parser.add_argument('--debug', \
                         help="Debug Mode", \
-                        action="store_true") 
+                        action="store_true")
 
     args=parser.parse_args()
     if args.verbose :
@@ -133,23 +166,23 @@ if __name__ == "__main__" :
         print("Data loading...")
     if args.ifile is None or not os.path.isfile(args.ifile) :
         print(f"Error : {args.ifile} is not a file")
-        exit()
+        sys.exit()
     else :
         DATA = pd.read_csv("QueryResults.csv")
         for param in ['Body', 'Tags'] :
             if param not in DATA.columns :
                 print(f"Error : {param} is not in the DataFrame")
-                exit()
+                sys.exit()
         if args.verbose or args.debug :
             print(f"Number of rows : {len(DATA)}")
             print(f"Number of variable : {len(DATA.columns)}")
         if args.debug :
             print(DATA)
-    
+
     #=========================
     # Process tags list
     #=========================
-    if args.verbose or args.debug : 
+    if args.verbose or args.debug :
         print("Tags processing...")
     tags_tokenizer = nltk.SExprTokenizer(parens='<>')
     DATA['Tags_list'] = DATA['Tags'].apply(tags_tokenizer.tokenize)
@@ -192,17 +225,36 @@ if __name__ == "__main__" :
     #=========================
     if args.verbose or args.debug :
         print("Model fitting...")
-    use_pipeline = Pipeline(steps=[\
-                                   ('use_features_trans', UseTransformer(tokenizer=tokenizer, \
-                                                                         embedding=hub.load("https://tfhub.dev/google/universal-sentence-encoder/4"), \
-                                                                         batch_size=1)
-                                    ),\
-                                   ('SVM_classifier', SVC() )\
-                               ])
-    use_pipeline.fit(DATA['Body'].iloc[:2000], y_one[:2000])
+    use_pipeline = sklearn.pipeline.Pipeline(steps=[\
+                                                    ('use_features_trans', \
+                                                     UseTransformer(tokenizer=tokenizer, \
+                                                                    embedding=hub.\
+                                                                    load("https://tfhub.dev/google/universal-sentence-encoder/4"), \
+                                                                    batch_size=1)
+                                                 ),\
+                                                    ('SVM_classifier', sklearn.svm.SVC() )\
+                                                ])
 
+    #=========================
+    # Training
+    #=========================
+    X_train, X_test, y_train, y_test = train_test_split(DATA['Body'].iloc[:200], \
+                                                        y_one[:200], \
+                                                        test_size=0.3,
+                                                        random_state=42)
+    use_pipeline.fit(X_train, y_train)
+
+    y_pred_train = use_pipeline.predict(X_train)
+    y_pred_test = use_pipeline.predict(X_test)
     if args.debug :
-        y_pred = use_pipeline.predict(DATA['Body'].iloc[:2000])
-        print(f"y_pred = {y_pred}")
-    print(f"Accuracy : {accuracy_score(y_one[:2000], y_pred)}")
-    #Make a pipeline : creation des features, fit, prediction, retour de la liste des tags en format str
+        print(f"y_pred_train = {y_pred_train}")
+        print(f"y_pred_test = {y_pred_test}")
+    print(f"Accuracy train set: {accuracy_score(y_train, y_pred_train)}")
+    print(f"Accuracy test set: {accuracy_score(y_test, y_pred_test)}")
+
+    #=========================
+    # save pipeline
+    #=========================
+    #with open('pipeline_use-svc.pkl', 'wb') as ofile :
+    #    pickle.dump(use_pipeline, ofile, pickle.HIGHEST_PROTOCOL)
+    #joblib.dump(use_pipeline, 'pipeline_use-svc.joblib')
