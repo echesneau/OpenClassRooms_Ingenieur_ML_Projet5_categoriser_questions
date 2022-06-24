7e592df21bb554d9f670200c225a36c22227a465
Erwan Chesneau <erwanchesneau@gmail.com>
2022-06-24 16:53:04 +0200

    Création d'une classe pour crééer les features et création du pipeline sklearn

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 #=========================
 # Python's Module Loading 
 #=========================
+import warnings
+warnings.filterwarnings("ignore")
+import logging
+logging.disable(logging.WARNING)
+
 import argparse
 import os
 import pickle
@@ -12,13 +17,15 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
+from sklearn.pipeline import Pipeline 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score
 
 import tensorflow as tf
 tf.get_logger().setLevel('INFO')
 import tensorflow_hub as hub
 
-import logging
-logging.disable(logging.WARNING)
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 #=========================
@@ -62,6 +69,40 @@ def use_features(sentences, embedding, batch_size=16):
             features = np.concatenate((features,feat))
     return features
     
+class UseTransformer() :
+    def __init__(self, tokenizer=None, embedding=None, batch_size=1) :
+        self.batch_size = batch_size
+        self.X_tokenized = None
+        if tokenizer is None :
+            self.tokenizer = self.dummy
+        else :
+            self.tokenizer = tokenizer
+        if embedding is not None :
+            self.embedding = embedding
+        else : 
+            print("Error : Please give me embedding")
+            exit()
+
+    def transform(self, X, y=None) :
+        self.X_tokenized = X.apply(self.tokenizer)
+        sentences = self.X_tokenized.to_list()
+        for step in range(len(sentences)//self.batch_size) :
+            idx = step*self.batch_size
+            feat = self.embedding(sentences[idx:idx+self.batch_size])
+            if step ==0 :
+                features = feat
+            else :
+                features = np.concatenate((features,feat))
+        return features
+    
+    def fit(self, X, y=None) :
+        return self
+
+    def dummy(self, txt):
+        """
+        function to return the txt without processing
+        """
+        return txt
 
 if __name__ == "__main__" :
     #=========================
@@ -139,13 +180,29 @@ if __name__ == "__main__" :
     #=========================
     # USE features
     #=========================
-    embedding = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-    DATA['Body_tokenized'] = DATA['Body'].apply(tokenizer)
     if args.debug :
-        print(DATA['Body_tokenized'])
-    features = use_features(DATA['Body_tokenized'].to_list(), embedding, batch_size=5)
-    if args.verbose or args.debug :
+        features = UseTransformer(tokenizer=tokenizer, \
+                                  embedding=hub.load("https://tfhub.dev/google/universal-sentence-encoder/4"), \
+                                  batch_size=1).transform(DATA['Body'])
         print(f"Shape of USE features : {features.shape}")
-    if args.debug :
         print(features)
+
+    #=========================
+    # Pipeline
+    #=========================
+    if args.verbose or args.debug :
+        print("Model fitting...")
+    use_pipeline = Pipeline(steps=[\
+                                   ('use_features_trans', UseTransformer(tokenizer=tokenizer, \
+                                                                         embedding=hub.load("https://tfhub.dev/google/universal-sentence-encoder/4"), \
+                                                                         batch_size=1)
+                                    ),\
+                                   ('SVM_classifier', SVC() )\
+                               ])
+    use_pipeline.fit(DATA['Body'].iloc[:2000], y_one[:2000])
+
+    if args.debug :
+        y_pred = use_pipeline.predict(DATA['Body'].iloc[:2000])
+        print(f"y_pred = {y_pred}")
+    print(f"Accuracy : {accuracy_score(y_one[:2000], y_pred)}")
     #Make a pipeline : creation des features, fit, prediction, retour de la liste des tags en format str
